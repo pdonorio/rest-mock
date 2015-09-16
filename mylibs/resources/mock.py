c9b0c44c1b5de8e63e8265bc9bb0dcf9bc30ea05
paulie <tempo.pause@gmail.com>
2015-09-16 10:54:21 +0200

    Adding an example of logging inside mock

--- mylibs/resources/mock.py
+++ mylibs/resources/mock.py
@@ -3,6 +3,8 @@
 
 """ Mock Resource """
 
+import logging
+logger = logging.getLogger(__name__)
 from mylibs.resources.base import ExtendedApiResource, returnstandarddata
 
 class Foo(ExtendedApiResource):
@@ -10,4 +12,5 @@ class Foo(ExtendedApiResource):
 
     @returnstandarddata
     def get(self):
+        logger.debug("Get request")
         return {'hello': 'world'}
