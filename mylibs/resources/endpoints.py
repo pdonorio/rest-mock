9da8444b1aafe1f16ffb3aad80c8da87c9c8fa6a
paulie <tempo.pause@gmail.com>
2015-09-17 13:23:25 +0200

    Refactoring: moving endpoints creation inside a class.

---
+++ mylibs/resources/endpoints.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" How to create endpoints into REST service """
+
+import logging
+logger = logging.getLogger(__name__)
+
+from mylibs.meta import Meta
+from mylibs.resources import FIXED_APIURL
+
+class Endpoints(object):
+    """ Handling endpoints creation"""
+
+    rest_api = None
+
+    def __init__(self, api):
+        super(Endpoints, self).__init__()
+        self.rest_api = api
+
+    def create_single(self, resource, endpoint, endkey):
+        """ Adding a single restpoint from a Resource Class """
+
+        address = FIXED_APIURL + endpoint
+        logger.info("Adding '%s' resource to REST address: *%s*", \
+            resource.__name__, address)
+
+        # Create restful endpoint
+        self.rest_api.add_resource(resource, \
+            FIXED_APIURL+endpoint,\
+            FIXED_APIURL + endpoint +'/<'+ endkey +'>')
+
+    def many_from_module(self, module):
+        """ Automatic creation of endpoint from specified resources """
+
+        resources = Meta().get_new_classes_from_module(module)
+        # Init restful plugin
+        if len(resources) > 0:
+            # For each RESTful resource i receive
+            for resource in resources:
+                endpoint, endkey = resource().get_endpoint()
+                self.create_single(resource, endpoint, endkey)
--- mylibs/server.py
+++ mylibs/server.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-""" Flask hello world """
+"""
+Main server code logic for this project
+"""
 
 import logging
 logger = logging.getLogger(__name__)
@@ -24,7 +26,8 @@ CORS(microservice, headers=['Content-Type'])
 from flask_restful import Api
 rest_api = Api(microservice, catch_all_404s=True)
 # automatic Resources
-from mylibs.resources import create_endpoint, create_endpoints
+from mylibs.resources.endpoints import Endpoints
+epo = Endpoints(rest_api)
 # Verify configuration
 from mylibs.config import MyConfigs
 resources = MyConfigs().rest()
@@ -33,11 +36,15 @@ resources = MyConfigs().rest()
 if len(resources) < 1:
     logger.info("No file configuration found (or no section inside)")
     from mylibs.resources import mock as mymodule
-    create_endpoints(rest_api, mymodule)
+    epo.many_from_module(mymodule)
 # Advanced: from a module mock
 else:
     logger.info("Using resources found in configuration")
 
     for myclass, instance, endpoint, endkey in resources:
         # Load each resource
-        create_endpoint(rest_api, myclass, endpoint, endkey)
+        epo.create_single(myclass, endpoint, endkey)
+
+####################################
+# The End?
+####################################
