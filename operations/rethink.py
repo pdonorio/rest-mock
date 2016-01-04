311ffb3103991e3f23e6b974529e05ce718fdbfc
Paulie D <p.donoriodemeo@gmail.com>
2016-01-04 01:12:08 +0100

    A complete conversion with Rethinkdb schema. Understanding the shape of JSON, nested fields, and how to search through them.

--- operations/rethink.py
+++ operations/rethink.py
@@ -10,6 +10,7 @@ from __future__ import absolute_import
 import logging
 from restapi.resources.services.rethink import RethinkConnection, RDBquery
 from restapi import get_logger
+from rethinkdb.net import DefaultCursorEmpty
 
 logger = get_logger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -17,9 +18,8 @@ logger.setLevel(logging.DEBUG)
 # Tables
 t1 = "stepstemplate"
 t2 = "steps"
-tin = "newsteps"
-
 t3 = "stepscontent"
+tin = "newsteps"
 t2in = "newstepscontent"
 
 # Connection
@@ -34,28 +34,60 @@ def convert_search():
     qt2 = query.get_table_query(tin)
     qtin = query.get_table_query(t2in)
     qtin.delete().run()
+    logger.info("Clear table '%s'" % t2in)
 
-    # DO
-    data = {}
-    for row in qt1.run():
-        print(row)
-        for myhash in row['hashes']:
-            print(myhash)
-            results = qt2.filter(
-                lambda x: x['fields']['original_hash'].contains(myhash)
-                ).run()
-            print([x['fields'] for x in results].pop())
-            # check = qt2.filter({'fields':
-            #     lambda x: x['original_hash'] == myhash }).run()
-            # if check:
-            #     for x in check:
-            #         print(x)#, x['name'], x['step'])
-            # else:
-            #     print("Cannot find", myhash)
-            #     exit(1)
-            # #print(qt2.filter({'hash': myhash}).run())
-            exit(1)
-        exit(1)
+    # Query
+    res = qt1.group('recordid').order_by('step').run()
+
+    for record, rows in res.items():
+        steps = []
+        for row in rows:
+
+            # Compose back the elements...
+            index = 0
+            elements = []
+            for myhash in row['hashes']:
+                # Query with lambda. COOL!
+                cursor = qt2.filter(
+                    lambda x: x['fields']['original_hash'].contains(myhash)
+                    ).run()
+                fields = []
+                try:
+                    fields = cursor.next()['fields']
+                except DefaultCursorEmpty:
+                    logger.warning("No original hash for '%s'" % myhash)
+
+                for field in fields:
+                    if field['original_hash'] == myhash:
+                        value = None
+                        try:
+                            value = row['values'][index]
+                        except:
+                            pass
+                        elements.append({
+                            'name': field['name'],
+                            'position': field['position'],
+                            'hash': field['original_hash'],
+                            'value': value,
+                        })
+                        break
+                index += 1
+
+            # Create a sane JSON to contain all the data for one step
+            steps.append({
+                'step': row['step'],
+                # Extra info
+                'latest_db_info': {
+                    'timestamp': row['latest_timestamp'],
+                    'ip': row['latest_ipaddress'],
+                    'user_id': row['user'],
+                },
+                'data': elements,
+            })
+
+        # Save the record
+        qtin.insert({'record': record, 'steps': steps}).run()
+        logger.info("Worked off document '%s'" % record)
 
 
 def convert_submission():
@@ -79,27 +111,24 @@ def convert_submission():
                        "desc": element['description']}
 
         # Singles steps fields
-        tmp = []
+        element = []
         fields = list(qt1.filter(myfilter).run())
         sorted_fields = sorted(fields, key=lambda k: k['position'])
         for row in sorted_fields:
             if 'extra' not in row:
                 row['extra'] = None
 
-            tmp.append({
-                # 'position': row['position'],
-                # 'data': {
-                    "name": row['field'],
-                    "position": row['position'],
-                    "required": row['required'],
-                    "type": row['type'],
-                    "options": row['extra'],
-                    "original_hash": row['hash'],
-                # }
+            element.append({
+                "name": row['field'],
+                "position": row['position'],
+                "required": row['required'],
+                "type": row['type'],
+                "options": row['extra'],
+                "original_hash": row['hash'],
             })
 
         # A whole step
-        new["fields"] = tmp
+        new["fields"] = element
 
         # INSERT
         logger.debug("To insert!\n%s" % new)
