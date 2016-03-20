5b088fe4f8835d7b88d171bf6959ea5f7ab84e1b
pdonorio <tempo.pause@gmail.com>
2016-03-20 15:35:23 +0100

    Recovering user and making operations upon

--- restapi/resources/profiles.py
+++ restapi/resources/profiles.py
@@ -6,9 +6,12 @@ Profiles for internal users
 
 from __future__ import division, absolute_import
 from .. import myself, lic, get_logger
+
 from .base import ExtendedApiResource
+from ..models import db, User, Role
 from . import decorators as deck
 from .. import htmlcodes as hcodes
+from confs import config
 
 __author__ = myself
 __copyright__ = myself
@@ -29,5 +32,26 @@ class InitProfile(ExtendedApiResource):
             return self.response(
                 "No user identifier specified to init the profile",
                 fail=True, code=hcodes.HTTP_DEFAULT_SERVICE_FAIL)
+
+        ######################
+        user = User.query.get(int(j[key]))
+        print("TEST USER", user)
+        user.first_name = 'pippo'
+        user.last_name = 'xyz'
+
+        ######################
+        # ### Bypassing with email confirmation
+        # # Not active after registration
+        # user.active = False
+
+        ######################
+        # Base role
+        role = Role.query.filter_by(name=config.ROLE_USER).first()
+        user.roles.append(role)
+
+        ######################
+        # Commit modifications
+        db.session.add(user)
+        db.session.commit()
 
         return "Yet to do"
