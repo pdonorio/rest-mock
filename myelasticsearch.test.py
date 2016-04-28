b040fe97dac8a607883a3f9f1985e3229881ebf4
pdonorio <tempo.pause@gmail.com>
2016-04-28 15:24:23 +0200

    Testing elasticsearch on ipython

---
+++ myelasticsearch.test.py
@@ -0,0 +1,42 @@
+# IPython log file
+
+from elasticsearch import Elasticsearch
+
+index = 'test'
+content1 = {
+    'test': 'There is <i>some</i> <b>HTML</b>',
+    'hello': 'World'
+}
+content2 = {
+    'test': 'There is <a href="">some other</a> <b>HTML</b>',
+    'hello': 'World2'
+}
+
+es = Elasticsearch(host='el')
+es.info()
+es.indices.create(index=index)
+es.indices.stats()
+
+es.index(index=index, id=1, body=content1, doc_type='some')
+es.index(index=index, id=2, body=content2, doc_type='some')
+
+es.get(index=index, doc_type='some', 1)
+
+# get all
+es.search(index=index)
+
+# query all fields, lowercase, no html
+es.search(index=index, body={"query": {"match": {"test": "some"}}})
+es.search(index=index, body={"query": {"match": {"test": "some HTML"}}})
+
+# Update existing
+es.update(index=index,
+          id=1, body={"doc": {'hello': 'no world'}}, doc_type='some')
+es.search(index=index)
+
+# Add a new field
+es.update(index=index, id=1, body={"doc": {'trans': 'test'}}, doc_type='some')
+es.search(index=index)
+
+# Limit output
+es.search(index=index, size=1)
