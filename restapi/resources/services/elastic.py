8d35a3fc25025f5b35ef8cebf8c0d673d2d5a5ce
pdonorio <tempo.pause@gmail.com>
2016-04-29 15:02:01 +0200

    Allowing get/search for elastic query

--- restapi/resources/services/elastic.py
+++ restapi/resources/services/elastic.py
@@ -6,7 +6,10 @@ from elasticsearch import Elasticsearch
 from ... import get_logger
 
 logger = get_logger(__name__)
+
 SERVER = 'el'
+EL_INDEX1 = 'autocomplete'
+EL_TYPE1 = 'data'
 
 
 # ######################################
@@ -18,4 +21,14 @@ class FastSearch(object):
 
     def get_instance(self):
         self._api = Elasticsearch(host=SERVER)
+        logger.info("Connected to Elasticsearch")
         return self
+
+    def fast_get(self, id):
+        args = {'index': EL_INDEX1, 'doc_type': EL_TYPE1}
+        if id is not None:
+            args['id'] = id
+            return self._api.get(**args)
+        else:
+            out = self._api.search(**args)
+            return out['hits']['hits']
