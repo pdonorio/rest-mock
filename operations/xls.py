e880b6e9b713b10e6bbbe3ad83f02748e0a27339
pdonorio <tempo.pause@gmail.com>
2016-06-08 17:53:08 +0200

    Updating to latest informations

--- operations/xls.py
+++ operations/xls.py
@@ -62,11 +62,9 @@ class ExReader(object):
 
         row_num = 0
         languages = []
-        # A list
         terms = []
-        latest_macro = latest_micro = "-undefined-"
-
-## WS TITLE = Macro of macro
+        latest_micro = None
+        latest_macro = "-undefined-"
 
         for row in ws.rows:
             data = list(row)
@@ -85,21 +83,25 @@ class ExReader(object):
             macro, micro, convention = block
 
             if row_num > 1:
-# Error if latest_macro is None
+                # Macro update
                 if macro is not None and macro.strip() != '':
-                    latest_macro = macro
-# micro = macro if None
+                    latest_macro = macro.strip().lower()
+                if latest_macro is None:
+                    raise KeyError("Empty macro inside '%s' sheet" % ws.title)
+                # Micro update
                 if micro is not None and micro.strip() != '':
-                    latest_micro = micro
+                    latest_micro = micro.strip().lower()
+                if latest_micro is None:
+                    latest_micro = latest_macro
 
             cell_num = -1
 
             from collections import OrderedDict
             term = OrderedDict({
-## Note: convention should not be shown
-                'term': convention,
+                'term': convention,  # Note: convention should not be shown
                 'macro': latest_macro,
                 'micro': latest_micro,
+                'sheet': ws.title.strip().lower()
             })
             # print("TERM", term)
 
@@ -135,15 +137,18 @@ class ExReader(object):
                 terms.append(dict(term))
                 # terms.append(list(term.values()))
 
-            ## JUST FOR DEBUG
-            # if row_num > 15:
-            if False:
-                from tabulate import tabulate
-                table = tabulate(
-                    terms,  # headers=term.keys(),
-                    tablefmt="fancy_grid")
-                print(table)
-                break
+            # ## JUST FOR DEBUG
+            # if row_num > 2:
+            #     break
+
+            # # Note: this works with lists, not dictionary...
+            # if False:
+            #     from tabulate import tabulate
+            #     table = tabulate(
+            #         terms,  # headers=term.keys(),
+            #         tablefmt="fancy_grid")
+            #     print(table)
+            #     break
 
         return terms
 
