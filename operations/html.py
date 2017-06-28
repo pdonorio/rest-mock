067df6f598bed0c88df690bd57912ae6c6ad0190
Paulie D <tempopause@gmail.com>
2017-06-28 21:38:44 +0000

    fix html problems

--- operations/html.py
+++ operations/html.py
@@ -7,8 +7,10 @@ import lxml.etree
 def convert(html_text):
     try:
         document = lxml.html.document_fromstring(html_text)
-    except lxml.etree.ParserError:
+    except (lxml.etree.ParserError, lxml.etree.XMLSyntaxError, ValueError) as e:
         # empty document
-        return html_text
+        return ''
+
     raw_text = document.text_content()
     return raw_text
+
